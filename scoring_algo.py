04df25492d1629c2f2c960a14aa5f59026dc5072
Gergely Attila Kiss <corra971407@gmail.com>
2020-05-06 00:08:58 +0200

    correct the loop and take care of missing observations

--- scoring_algo.py
+++ scoring_algo.py
@@ -3,17 +3,14 @@ import numpy as np
 
 df=pd.read_csv("input/foreign_sample.csv")
 df['score'] = pd.Series( 0 , index = df.index)
+df.Partner = df.Partner.fillna('')
+df.Name = df.Name.fillna('')
 
 def levenshtein_score(string1, string2):
     score = 0
     i=0
     matching = 0
-    # set score maximum the length of the longer string
-    if string1 == NaN:
-        string1 = ''
-    if stirng2 == NaN:
-        string2 = ''
-        
+    # set score maximum the length of the longer string        
     if len(string1)>len(string2):
         score = len(string1)
         #make the strings equally long
@@ -28,13 +25,11 @@ def levenshtein_score(string1, string2):
         if string1[i] == string2[i]:
             matching = matching + 1    
         i = i + 1
-
+    #converts score to [0,1]
     score = 1-(score - matching)/len(string1)
     return score
 
     i = 0
-    #loop to create levenshtein score row-wise, not working properly yet
-    #missing observations are an issue yet
 	while i < len(df):
-    df.at[i, 'score'] = levenshtein_score(df.Partner[i], df.Name[i])
+    df.loc[i, 'score'] = levenshtein_score(df.Partner[i], df.Name[i])
     i = i+1
